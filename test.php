3d1053ad78aa24f7b21894cba5a9b9ac4a7ba647
David Gabrielian <49720192+DavGabrielian@users.noreply.github.com>
2020-05-19 12:51:27 +0400

    Update test.php

--- test.php
+++ test.php
@@ -1,6 +1,5 @@
 <?php
 
-session_start();
 
 include "connect.php";
 
@@ -36,6 +35,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 }
 
 
+
 if(empty($_SESSION['error']))
 {
 
@@ -46,7 +46,61 @@ if(empty($_SESSION['error']))
 
 
     if($sqlPost) {
+
+        //UPLOAD
+        if (isset($_POST['submit'])) {
+    
+            $targetDir = "uploads/";
+            $allowTypes = array('jpg', 'jpeg', 'png', 'gif');
+        
+            $statusMsg  = $insertValuesSQL  =  '';
+            $errorMsg = $errorUpload = $errorUploadType = '';
+            $fileNames = array_filter($_FILES['files']['name']);
+            if (!empty($fileNames)) {
+                foreach ($_FILES['files']['name'] as $key => $val) {
+        
+                    // upload path
+                    $fileName = basename($_FILES['files']['name'][$key]);
+                    $targetFilePath = $targetDir . $fileName;
+        
+                    // validation
+                    $fileType = pathinfo($targetFilePath, PATHINFO_EXTENSION);
+                    if (in_array($fileType, $allowTypes)) {
+                        // upload
+                        if (move_uploaded_file($_FILES["files"]["tmp_name"][$key], $targetFilePath)) {
+                            // insert sql 
+                            $insertValuesSQL .= "('" . $fileName . "', NOW()),";
+                        } else {
+                            $errorUpload .= $_FILES['files']['name'][$key] . ' | ';
+                        }
+                    } else {
+                        $errorUploadType .= $_FILES['files']['name'][$key] . ' | ';
+                    }
+                }
+                if (!empty($insertValuesSQL)) {
+                    $insertValuesSQL = trim($insertValuesSQL, ',');
+                    //  file name into database 
+                    $insert = $link->query("INSERT INTO product_images (file_name, uploaded_on) 
+                                            VALUES $insertValuesSQL");
+                    if ($insert) {
+                        $errorUpload = !empty($errorUpload) ? 'Upload Error: ' . trim($errorUpload, ' | ') : '';
+                        $errorUploadType = !empty($errorUploadType) ? 'File Type Error: ' . trim($errorUploadType, ' | ') : '';
+                        $errorMsg = !empty($errorUpload) ? '<br/>' . $errorUpload . '<br/>' . $errorUploadType : '<br/>' . $errorUploadType;
+                        $statusMsg = "Files are uploaded successfully." . $errorMsg;
+                    } else {
+                        $statusMsg = "Sorry, there was an error uploading your file.";
+                    }
+                }
+            } else {
+                $statusMsg = 'Please select a file to upload.';
+            }
+        
+            echo $statusMsg;
+        }
+
+        
         header("Location: index.php");
+        
     } else {
         //insert problem
         header("location: error.php"); 
@@ -59,60 +113,5 @@ if(empty($_SESSION['error']))
     header("Location: form.php");
     
 }
-
-//UPLOAD
-
-if (isset($_POST['submit'])) {
-    
-    $targetDir = "uploads/";
-    $allowTypes = array('jpg', 'jpeg', 'png', 'gif');
-
-    $statusMsg  = $insertValuesSQL  =  '';
-    $errorMsg = $errorUpload = $errorUploadType = '';
-    $fileNames = array_filter($_FILES['files']['name']);
-    if (!empty($fileNames)) {
-        foreach ($_FILES['files']['name'] as $key => $val) {
-
-            // upload path
-            $fileName = basename($_FILES['files']['name'][$key]);
-            $targetFilePath = $targetDir . $fileName;
-
-            // validation
-            $fileType = pathinfo($targetFilePath, PATHINFO_EXTENSION);
-            if (in_array($fileType, $allowTypes)) {
-                // upload
-                if (move_uploaded_file($_FILES["files"]["tmp_name"][$key], $targetFilePath)) {
-                    // insert sql 
-                    $insertValuesSQL .= "('" . $fileName . "', NOW()),";
-                } else {
-                    $errorUpload .= $_FILES['files']['name'][$key] . ' | ';
-                }
-            } else {
-                $errorUploadType .= $_FILES['files']['name'][$key] . ' | ';
-            }
-        }
-        if (!empty($insertValuesSQL)) {
-            $insertValuesSQL = trim($insertValuesSQL, ',');
-            //  file name into database 
-            $insert = $link->query("INSERT INTO product_images (file_name, uploaded_on) 
-                                    VALUES $insertValuesSQL");
-            if ($insert) {
-                $errorUpload = !empty($errorUpload) ? 'Upload Error: ' . trim($errorUpload, ' | ') : '';
-                $errorUploadType = !empty($errorUploadType) ? 'File Type Error: ' . trim($errorUploadType, ' | ') : '';
-                $errorMsg = !empty($errorUpload) ? '<br/>' . $errorUpload . '<br/>' . $errorUploadType : '<br/>' . $errorUploadType;
-                $statusMsg = "Files are uploaded successfully." . $errorMsg;
-            } else {
-                $statusMsg = "Sorry, there was an error uploading your file.";
-            }
-        }
-    } else {
-        $statusMsg = 'Please select a file to upload.';
-    }
-
-    echo $statusMsg;
-}
-
-
-
 
 
